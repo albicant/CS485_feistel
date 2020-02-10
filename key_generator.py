1bfc31c6ee706a718c339740fb661117383bfe18
albicant <silvics@protonmail.com>
2020-02-09 21:55:56 -0800

    Completed the key generation

--- key_generator.py
+++ key_generator.py
@@ -10,8 +10,8 @@ def shiftBitLeft(block):
     return block
 
 def createKeyArray(block):
-    key_array = [block]
-    for i in range(0, 63):
+    key_array = []
+    for i in range(0, 64):
         block = shiftBitLeft(block)
         key_array.append(block)
 
@@ -24,23 +24,62 @@ def breakHexIntoChunks(block):
     else:
         while(len(block) < 16):
             block = "0" + block
-    
-    return [block[i:i+2] for i in range(0, len(block), 2)]
+
+    block_list = []
+    i = len(block)
+    while(i > 0):
+        byte = "0x" + block[i - 2: i]
+        block_list.append(byte)
+        i -= 2
+    return block_list
          
+def generateKeys(key):
+    row_keys = createKeyArray(key)
+    keys = []
+    counter = -1
+    x = 0
+    while(x < 192 and counter < 16):
+        i = x % 64
+        byte_arr = breakHexIntoChunks(row_keys[i])
+        if(x % 12 == 0):
+            keys.append("")
+            counter += 1
+
+        b = x % 8
+        keys[counter] = keys[counter] + byte_arr[b] + " "
+        x += 1
+    
+    return keys
+        
+
+
+
+
+
 
 
 original_key = int(0xabcdef0123456789)
-new_key = shiftBitLeft(original_key)
-key_array = createKeyArray(original_key)
-print(len(key_array))
+# new_key = shiftBitLeft(original_key)
+# key_array = createKeyArray(original_key)
+# print(len(key_array))
 
 print(hex(original_key))
-print(hex(new_key))
 
-test_key = key_array[63]
-print(hex(test_key))
+keys = generateKeys(original_key)
+for key in keys:
+    str1 = key
+    print(str1)
+# print(hex(new_key))
+# print(hex(key_array[2]))
 
-block = int(0xabcdef01234567890)
-block = breakHexIntoChunks(block)
-print(len(block))
+# test_key = key_array[63]
+# print(hex(test_key))
+
+# block = int(0xabcdef01234567890)
+# block = breakHexIntoChunks(block)
+# print(len(block))
+# str1 = ""
+# for i in range(0, len(block)):
+#     str1 += block[i] + " "
+# print(str1)
 # print("BreakHexIntoChunks:" + block)
